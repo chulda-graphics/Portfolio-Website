f9282732353ddb100063d565f337dc1f24b83e8d
Dhrex Cañezo <chulda.graphics2022@gmail.com>
2026-07-29 19:22:11 +0800

    Clean website repository for fresh rebuild

--- components/site-footer.tsx
+++
@@ -1,9 +0,0 @@
-export function SiteFooter() {
-  return (
-    <footer className="site-footer">
-      <p>Dhrex / 2026</p>
-      <p>Freelance · Remote worldwide</p>
-      <a href="mailto:chulda.graphics2022@gmail.com">Email Dhrex ↗</a>
-    </footer>
-  );
-}
